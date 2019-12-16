dc3c979df71e492868008189092d8c04bd1d8687
vivek gupta <vivek@viveks-MacBook-Pro.local>
2019-12-16 05:07:49 -0500

    lab 2

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,19 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    private String msg;
+    public WelcomeController(@Value("${welcome.message}") String mess) {
+     msg=mess;
+    }
+
     @GetMapping("/")
     public String sayHello(){
-        return "hello";
+        return msg;
     }
 }
